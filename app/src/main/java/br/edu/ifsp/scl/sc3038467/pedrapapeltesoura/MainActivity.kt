13d5cc634be0e948fcbf93e874eb892305119835
Murilo Corrêa <murilosao06@gmail.com>
2026-03-27 16:31:52 -0300

    feat: declara variaveis da interface

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/pedrapapeltesoura/MainActivity.kt
@@ -1,20 +1,35 @@
 package br.edu.ifsp.scl.sc3038467.pedrapapeltesoura
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rgJogadores: RadioGroup
+    private lateinit var ivApp1: ImageView
+    private lateinit var ivApp2: ImageView
+    private lateinit var tvResultado: TextView
+
+    private val motor = MotorDoJogo()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        rgJogadores = findViewById(R.id.rgJogadores)
+        ivApp1 = findViewById(R.id.ivApp1)
+        ivApp2 = findViewById(R.id.ivApp2)
+        tvResultado = findViewById(R.id.tvResultado)
+
+        rgJogadores.setOnCheckedChangeListener { _, checkedId ->
+            ivApp2.visibility = if (checkedId == R.id.rbTresJogadores) View.VISIBLE else View.GONE
         }
     }
 }
